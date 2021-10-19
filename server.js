4a07b7de6096e07af90d7e0114d285c9a478f913
Pat Ranile <fatimaranile@gmail.com>
2021-10-19 20:52:18 +0800

    Add start script to package.json. rename express app.

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const cors = require("cors");
 const express = require("express");
 
-const database = require("./src/models");
+//const database = require("./src/models");
 
 const corsOptions = {
   origin: "http://localhost:8081",
 };
 
-const expressApp = express();
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-expressApp.use(cors(corsOptions));
-expressApp.use(express.json());
-expressApp.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // sample GET endpoint
-expressApp.get("/", (request, response) => {
+app.get("/", (request, response) => {
   response.json({
     message: "Daily Scrum API is running.",
   });
 });
 
-database.sequelize.sync();
+//database.sequelize.sync();
 
-expressApp.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
